2aad477c4a7877f5b750523a6d14b2e4de43e917
Jacob Pedersen <jacob.bp@mvb.net>
2018-04-15 17:36:43 +0200

    Update Eksempel1_DC_Motor_Driver.ino

--- Del_2/Eksempel1_DC_Motor_Driver/Eksempel1_DC_Motor_Driver.ino
+++ Del_2/Eksempel1_DC_Motor_Driver/Eksempel1_DC_Motor_Driver.ino
@@ -1,4 +1,4 @@
-#define MOTORPIN 9
+#define MOTORPIN 5
 #define AIN A0
 
 int potRead = 0;
@@ -13,6 +13,6 @@ void loop(){
 	potRead = analogRead(AIN);
 	motorSpeed = potRead/4;
 	analogWrite(MOTORPIN, motorSpeed);
-	Serial.print("potRead = "); Serial.println(potRead); // potReads værdi skrives ud på den serielle forbindelse.
-  Serial.print("motorSpeed = "); Serial.println(motorSpeed); // servoPos' værdi skrives ud på den serielle forbindelse.
+	Serial.print("potRead = "); Serial.println(potRead); // potReads vÃ¦rdi skrives ud pÃ¥ den serielle forbindelse.
+  Serial.print("motorSpeed = "); Serial.println(motorSpeed); // servoPos' vÃ¦rdi skrives ud pÃ¥ den serielle forbindelse.
 }
