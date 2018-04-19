d2c2bc3ca5d8453dbb87fca7990995e797c5de0f
Jacob Bechmann Pedersen <jacob.bp@mvb.net>
2018-04-19 16:02:38 +0200

    Added HM-10

---
+++ Del_3/Eksempel4_HM_10_setup/Eksempel4_HM_10_setup.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+#define EN 9 // EN bruges til at sætte HC-05 i AT mode.
+
+SoftwareSerial HM10(10,11); // HC05 pins - RX pin 11, TX pin 10
+
+void setup() {
+  
+  pinMode(EN, OUTPUT);
+  digitalWrite(EN, HIGH);
+  Serial.begin(9600); // Start serial kommunikation med computeren.
+  Serial.println("Enter AT-Commands:"); // Prompt for AT kommandoer.
+  HM10.begin(9600); // S?tter HC05 serial hastigheden til 38400 baud. AT mode standardhastighed.
+
+}
+
+void loop() {
+  
+  // Læser fra HM10 forbindelsen, og skriver det til Arduinos serial.
+  if(HM10.available()){
+    Serial.write(HM10.read());
+  }
+  
+  // Læser fra Arduino, og sender til HC05:
+	// AT-Commands kan findes i en HC-05_AT_commands.pdf
+	// Giv modulet et navn med AT+NAME[navn]
+	// Sæt et nyt kodeord vha. AT+PASS[kode]
+	// Sæt baud-rate til 115200: AT+BAUD=4
+  // Sæt stop bit til 1: AT+STOP=0
+  // Sæt Parity til ingen: AT+BAUD=0
+	// HUSK at anvende Carriage return OG newline i Serial forbindelsen.
+  if(Serial.available()){
+    HM10.write(Serial.read());
+  }
+  
+}
